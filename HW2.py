082d3d7dd1203d8d9909c3f987b30b7231ec91b9
Andrew Sosanya <sosa@Andrews-MacBook-Pro-10.local>
2018-10-02 18:53:04 -0400

    HW2

--- HW2.py
+++ HW2.py
@@ -1,3 +1,4 @@
+import cs1lib
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -38,9 +39,9 @@ def draw_robot():
     jprime[2] = [robdiam, robdiam]
 
     inv_jprime = pinv(jprime)
-    print inv_jprime.shape
+
     wheel_vec = (2/wheel_rad) *  np.dot(inv_jprime, q)
-    print wheel_vec
+
 
 
 
@@ -55,14 +56,60 @@ draw_robot()
 
 
 class Robot:
-    def init(self, x = 0, y = 0 , theta = 0, x_p = 0, y_p = 0):
+    def init(self, x=0, y=0 , theta=0, x_p=0, y_p=0, wheel_rad = 0.003, robdiam = 0.05):
         self.x = x
         self.y = y
         self.theta = theta
         self.x_p = 0
         self.y_p = 0
+        self.wheel_rad = wheel_rad
+        self.wheel_vel = 0
+        self.robdiam = robdiam
 
-    def update_velocity(self, q):
+    def update_position(self, q):
         self.x += q[0]
         self.y += q[1]
         self.theta = q[2]
+
+
+    def draw_pen(self):
+        draw_line(self.x, self.x + 5*np.cos(self.theta), self.y, self.y +5*np.cos(self.theta))
+    def draw_robot(self):
+        #draw pen
+        draw_line(self.x, self.x + 5*np.cos(self.theta), self.y, self.y +5*np.cos(self.theta))
+        #draw robot as pointmass
+        draw_circle(self.x,self.y)
+        vx_p = 0
+        vy_p = 0
+
+        theta = 0
+        x_p = x + np.cos(theta)
+        y_p = y + np.sin(theta)
+        dxpdx = 1
+        dxpdy = 0
+        dxpd0= -np.sin(theta)
+        dypdx = 0
+        dypdy = 1
+        dypd0= np.cos(theta)
+        robdiam = 0.05
+        wheel_rad = 0.03
+
+        Jac = np.zeros(shape=(2,3))
+
+        Jac[0] = [dxpdx,dxpdy,dxpd0]
+        Jac[1] = [dypdx,dypdy,dypd0]
+
+        inv_Jac = pinv(Jac)
+
+        q = np.dot(inv_Jac,pen_vel(0,0))
+
+        jprime = np.zeros(shape=(3,2))
+        jprime[0] = [np.sin(theta), np.sin(theta)]
+        jprime[1] = [np.cos(theta), np.cos(theta)]
+        jprime[2] = [robdiam, robdiam]
+
+        inv_jprime = pinv(jprime)
+
+        #velocity vector for each of the wheels.
+        wheel_vec = (2/wheel_rad) *  np.dot(inv_jprime, q)
+        #def
